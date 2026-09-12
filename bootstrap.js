ef78299e7a698aa6419a250611b8aa3bc9f2e0c2
Emilin123 <clendifraga@gmail.com>
2026-09-12 01:11:32 -0500

    Remove obsolete legacy file bootstrap.js

--- bootstrap.js
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-import { execFileSync } from 'node:child_process';
-const target='/app/maison-aurea-final';
-if(!fs.existsSync(target+'/server.js')){
-  const b64=fs.readFileSync('/app/bundle.b64','utf8');
-  fs.writeFileSync('/tmp/maison-aurea.tar.gz',Buffer.from(b64,'base64'));
-  execFileSync('tar',['-xzf','/tmp/maison-aurea.tar.gz','-C','/app']);
-}
-await import('/app/maison-aurea-final/server.js');
